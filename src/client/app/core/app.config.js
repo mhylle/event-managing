76d90f23c60e6672b65ddfa5983124ec2d5b6dd0
mhylle <mhylle@yahoo.com>
2015-11-08 09:06:24 +0100

    Pre-alpha on login

---
+++ src/client/app/core/app.config.js
@@ -0,0 +1,19 @@
+//(function () {
+//    'use strict';
+//
+//    angular.module('app')
+//        .config(['$routeProvider', routes]);
+//    function routes($routeProvider) {
+//        $routeProvider
+//            .when('/login', {
+//                templateUrl: 'views/login.html',
+//                controller: 'SecurityController'
+//            })
+//            .otherwise({
+//                templateUrl: 'views/login.html',
+//                controller: 'SecurityController'
+//                //redirectTo: '/'
+//            });
+//    }
+//})
+//();
