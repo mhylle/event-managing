2ec57c54a70bc29df1da7745101db489cef3d812
mhylle <mhylle@yahoo.com>
2015-11-16 14:29:38 +0100

    Started to modularise the server code. Security has now been moved into it's own file. There are still throughput methods in the routes.js file. could this be done better?

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,11 +1,12 @@
 /* W0116 */
 var express = require('express');
-var errorhandler = require('errorhandler')
+var errorhandler = require('errorhandler');
 var router = express.Router();
-var jwt = require('jwt-simple');
+
 var four0four = require('./utils/404')();
+var security = require('./security/security')();
 var data = require('./data');
-var _ = require('underscore');
+
 
 var app = express();
 //app.set('jwtTokenSecret', '123456ABCDEF');
@@ -13,81 +14,24 @@ app.use(errorhandler);
 router.get('/users', requiresAuthentication, getUsers);
 router.get('/user/:id', requiresAuthentication, getUser);
 router.post('/login/', login);
-router.get('/logout/',  logout);
+router.get('/logout/', logout);
 router.get('/*', four0four.notFoundMiddleware);
 
 module.exports = router;
-var secret = '4757hgf87348gfhj3rf89fhj8rgerg345';
-var tokens = [];
+
 //////////////
 
 function login(req, res, next) {
-    var userName = req.body.userName;
-    var password = req.body.password;
-    var users = data.users;
-    var foundUser = false;
-    for (var i=0;i<users.length; i++) {
-        var user = users[i];
-        if (userName === user.username) {
-            if (password === user.passtring) {
-                var expires = new Date();
-                expires.setDate((new Date()).getDate() + 5);
-                var token = userName;
-                //var token = jwt.encode({
-                //    userName: userName,
-                //    expires: expires
-                //}, secret, 'HS512');
-                tokens.push(token);
-                foundUser = true;
-                res.send(200, {accesstoken: token, userName: userName});
-                break;
-            }
-        }
-    }
-    if (!foundUser) {
-        res.send(401, 'Invalid Credentials');
-    }
+    security.login(req, res, next);
 }
 
 function logout(req, res, next) {
-    var token = req.headers.accesstoken;
-    removeFromTokens(token);
-    res.send(200);
+    security.logout(req, res, next);
 }
 
-function removeFromTokens(token) {
-    for (var counter = 0; counter < tokens.length; counter++) {
-        if (tokens[counter] === token) {
-            tokens.splice(counter, 1);
-            break;
-        }
-    }
+function requiresAuthentication(req,res,next) {
+    security.requiresAuthentication(req,res,next);
 }
-function requiresAuthentication(request, response, next) {
-    //console.log(request.headers);
-    if (request.headers.accesstoken && request.headers.accesstoken != null) {
-        var token = request.headers.accesstoken;
-        // underscore checks if the token supplied is in the list of tokens that the server is aware of.
-        // if the token is found we can then start looking into if it is a valid one.
-        var where = _.where(tokens, token);
-        if (where.length > 0) {
-            //var decodedToken = jwt.decode(token, secret);
-            console.log(token);
-            next();
-            return;
-            //if (new Date(decodedToken.expires) > new Date()) {
-            //    next();
-            //    return;
-            //} else {
-            //    removeFromTokens();
-            //    response.status(401).send('Your session is expired');
-            //    return;
-            //}
-        }
-    }
-    response.status(401).send('No access token found in the request');
-}
-
 function getUsers(req, res, next) {
     console.log('getusers start');
     res.status(200).send(data.users);
---
+++ src/server/security/security.js
@@ -0,0 +1,81 @@
+var data = require('../data');
+var _ = require('underscore');
+var jwt = require('jwt-simple');
+var secret = '4757hgf87348gfhj3rf89fhj8rgerg345';
+var tokens = [];
+module.exports = function () {
+    var service = {
+        login: login,
+        logout: logout,
+        logout: logout,
+        requiresAuthentication: requiresAuthentication
+    };
+    return service;
+    
+    function login(req, res, next) {
+        var userName = req.body.userName;
+        var password = req.body.password;
+        var users = data.users;
+        var foundUser = false;
+        for (var i = 0; i < users.length; i++) {
+            var user = users[i];
+            if (userName === user.username) {
+                if (password === user.passtring) {
+                    //var expires = new Date();
+                    //expires.setDate((new Date()).getDate() + 5);
+                    var token = userName;
+                    //var token = jwt.encode({
+                    //    userName: userName,
+                    //    expires: expires
+                    //}, secret, 'HS512');
+                    tokens.push(token);
+                    foundUser = true;
+                    res.send(200, {accesstoken: token, userName: userName});
+                    return;
+                }
+            }
+        }
+        res.send(401, 'Invalid Credentials');
+    }
+
+    function logout(req, res, description) {
+        var token = req.headers.accesstoken;
+        removeFromTokens(token);
+        res.send(200);
+    }
+
+    function removeFromTokens(token) {
+        for (var counter = 0; counter < tokens.length; counter++) {
+            if (tokens[counter] === token) {
+                tokens.splice(counter, 1);
+                break;
+            }
+        }
+    }
+
+    function requiresAuthentication(request, response, next) {
+        //console.log(request.headers);
+        if (request.headers.accesstoken && request.headers.accesstoken != null) {
+            var token = request.headers.accesstoken;
+            // underscore checks if the token supplied is in the list of tokens that the server is aware of.
+            // if the token is found we can then start looking into if it is a valid one.
+            var where = _.where(tokens, token);
+            if (where.length > 0) {
+                //var decodedToken = jwt.decode(token, secret);
+                console.log(token);
+                next();
+                return;
+                //if (new Date(decodedToken.expires) > new Date()) {
+                //    next();
+                //    return;
+                //} else {
+                //    removeFromTokens();
+                //    response.status(401).send('Your session is expired');
+                //    return;
+                //}
+            }
+        }
+        response.status(401).send('No access token found in the request');
+    }
+
+};
