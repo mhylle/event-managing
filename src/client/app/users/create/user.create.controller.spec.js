b3782d86c9d2d93e3b1a618fbc80ab45f0e9c463
mhylle <mhylle@yahoo.com>
2015-12-12 10:45:38 +0100

    added a stupid test to see what TravisCI does if we do not fail on the tests.

--- src/client/app/users/create/user.create.controller.spec.js
+++ src/client/app/users/create/user.create.controller.spec.js
@@ -1,29 +1,29 @@
 ///* jshint -W117, -W030 */
-//describe('Controller: schema.user.UserCreateController', function () {
-//    it ('should just work', function () {
-//        expect(1).to.equal(1);
-//    });
-//    //// load the controller's module
-//    beforeEach(module('schema.user'));
-//    //
-//
-//    //beforeEach(function() {
-//    //    var myMocks = angular.module('MyAppMocks', []);
-//    //    myMocks.factory('')
-//    //})
-//    var ctrl, scope, lgr;
-//    ////
-//    ////// Initialize the controller and a mock scope
-//    beforeEach(inject(function ($controller, $rootScope, logger) {
-//        lgr = logger;
-//        scope = $rootScope.$new();
-//        ctrl = $controller('UserCreateController', {
-//            $scope: scope,
-//            logger: lgr
-//        });
-//    }));
-//    //
-//    it('should be defined', function () {
-//        expect(ctrl).toBeDefined();
-//    });
-//});
+describe('Controller: schema.user.UserCreateController', function () {
+    it ('should just work', function () {
+        expect(1).to.equal(1);
+    });
+    //// load the controller's module
+    //beforeEach(module('schema.user'));
+    //
+
+    //beforeEach(function() {
+    //    var myMocks = angular.module('MyAppMocks', []);
+    //    myMocks.factory('')
+    //})
+    //var ctrl, scope, lgr;
+    ////
+    ////// Initialize the controller and a mock scope
+    //beforeEach(inject(function ($controller, $rootScope, logger) {
+    //    lgr = logger;
+    //    scope = $rootScope.$new();
+    //    ctrl = $controller('UserCreateController', {
+    //        $scope: scope,
+    //        logger: lgr
+    //    });
+    //}));
+    //
+    //it('should be defined', function () {
+    //    expect(ctrl).toBeDefined();
+    //});
+});
