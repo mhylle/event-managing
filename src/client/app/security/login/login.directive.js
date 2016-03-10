112b6e9bb677f69bf8a7ee436dc00329fe2be3ea
mhylleberg@csc.com <mhylle@yahoo.com>
2016-03-10 10:03:17 +0100

    user service is now fully tested

--- src/client/app/security/login/login.directive.js
+++ src/client/app/security/login/login.directive.js
@@ -1,30 +1,30 @@
-(function () {
-    'use strict';
-
-    angular
-        .module('event-managing-security')
-        .directive('loginDialog', loginDialog);
-
-    loginDialog.$inject = ['AUTH_EVENTS'];
-
-    /* @ngInject */
-    function loginDialog(AUTH_EVENTS) {
-        var directive = {
-            bindToController: true,
-            link: link,
-            restrict: 'A',
-            template: '<div ng-if="visible" ng-include="\'login.html\'">',
-            scope: {}
-        };
-        return directive;
-
-        function link(scope) {
-            var showDialog = function () {
-                scope.visible = true;
-            };
-            scope.visible = false;
-            scope.$on(AUTH_EVENTS.notAuthenticated, showDialog);
-            scope.$on(AUTH_EVENTS.sessionTimeout, showDialog);
-        }
-    }
-})();
+//(function () {
+//    'use strict';
+//
+//    angular
+//        .module('event-managing-security')
+//        .directive('loginDialog', loginDialog);
+//
+//    loginDialog.$inject = ['AUTH_EVENTS'];
+//
+//    /* @ngInject */
+//    function loginDialog(AUTH_EVENTS) {
+//        var directive = {
+//            bindToController: true,
+//            link: link,
+//            restrict: 'A',
+//            template: '<div ng-if="visible" ng-include="\'login.html\'">',
+//            scope: {}
+//        };
+//        return directive;
+//
+//        function link(scope) {
+//            var showDialog = function () {
+//                scope.visible = true;
+//            };
+//            scope.visible = false;
+//            scope.$on(AUTH_EVENTS.notAuthenticated, showDialog);
+//            scope.$on(AUTH_EVENTS.sessionTimeout, showDialog);
+//        }
+//    }
+//})();
--- src/client/app/users/user.service.spec.js
+++ src/client/app/users/user.service.spec.js
@@ -12,27 +12,30 @@ describe('UserService', function () {
     });
 
     describe('getUsers', function () {
-        beforeEach(function () {
-            $httpBackend.expectGET('/api/user').respond(
-                users
-            );
-        });
-        it('returns a value', function () {
-            var userResult = userservice.getUsers().then(function (response) {
-                userResult = response;
+        describe('success', function () {
+            beforeEach(function () {
+                $httpBackend.expectGET('/api/user').respond(
+                    users
+                );
             });
-            expect(userResult).to.exists;
-        });
-        it('Retrieves an amount of users', function () {
-            var userResult = [];
-            userservice.getUsers().then(function (results) {
-                userResult = results.users;
+
+            it('returns a value', function () {
+                var userResult = userservice.getUsers().then(function (response) {
+                    userResult = response;
+                });
+                expect(userResult).to.exists;
             });
-            $httpBackend.flush();
-            expect(userResult).to.have.length.above(0);
+            it('Retrieves an amount of users', function () {
+                var userResult = [];
+                userservice.getUsers().then(function (results) {
+                    userResult = results.users;
+                });
+                $httpBackend.flush();
+                expect(userResult).to.have.length.above(0);
+            });
         });
 
-        describe.skip('Failure', function () {
+        describe('Failure', function () {
             it('Should log an error if the server returns an error', function () {
                 $httpBackend.expectGET('/api/user').respond(500);
                 var userResult = [];
@@ -114,11 +117,62 @@ describe('UserService', function () {
             });
         });
     });
+    describe('createUser', function () {
+        describe('Success', function () {
+            it('Should create a user', function () {
+                $httpBackend.expectPOST('/api/user/').respond(
+                    {status: 'ok', info: '', user: users.users[0]}
+                );
+                var userResult = [];
+                userservice.createUser(users.users[0]).then(function (results) {
+                    userResult = results.user;
+                });
+                $httpBackend.flush();
+                expect(userResult).to.exists;
+            });
+
+            it('Should not be able to create a user that already exists', function () {
+                var user1;
+                var user2;
+
+                user1 = getUser(1);
+                user2 = getUser(1);
+
+                expect(user1.id).to.equal(user2.id);
+            });
+        });
+
+        describe('Failure', function () {
+            it('Should log an error if the server returns an error', function () {
+                $httpBackend.expectPOST('/api/user/').respond(500);
+                var userResult = [];
+                userservice.createUser(users.users[0]).then(function (results) {
+                    userResult = results;
+                });
+
+                $httpBackend.flush();
+                expect(userResult).not.to.exist;
+                expect($log.error.logs).not.to.be.empty;
+                expect($log.error.logs).not.to.be.undefined;
+            });
+
+            it('Should return an error value if the server returns an error', function () {
+                $httpBackend.expectGET('/api/user/id/1').respond(500, {status: 'failed', info: 'no user found'}
+                );
+                var userResult = [];
+                userservice.getUser(1).then(function (results) {
+                    userResult = results;
+                });
+                $httpBackend.flush();
+                expect(userResult).not.to.exist;
+            });
+        });
+    });
 
     function getUser(id) {
         var user = {};
         $httpBackend.expectGET('/api/user/id/' + id).respond(
-            {status: 'ok', info:'', user: users.users[id - 1]}
+            {status: 'ok', info: '', user: users.users[id - 1]}
         );
         userservice.getUser(id).then(function (results) {
             user = results;
